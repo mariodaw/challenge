a32de88ea7119451e945f7a5c23d1a6559dec8d2
karensalazar3 <karensalazar0503@gmail.com>
2024-12-03 11:57:27 +0100

    cambios

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+
 const { dbConnection } = require("./config/config")
 
 app.use(express.json())
+
+app.use("/tasks",require("./routes/tasks"))
+
+
 
 dbConnection()
 
---
+++ models/Tasks.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.SchemaTypes.ObjectId;
+
+const TaskSchema = new mongoose.Schema({
+    title: String,
+    completed: Boolean,
+}, { timestamps: true });
+
+TaskSchema.index({ title: "text" });
+
+const Task = mongoose.model('Task', TaskSchema);
+
+module.exports = Task;
