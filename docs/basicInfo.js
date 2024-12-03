7862c76b2f2680c3a8ca8d47cd02e445d39e020c
fullstackLuis91 <luis_rv91@hotmail.com>
2024-12-03 12:24:18 +0100

    la documentacion es basura

---
+++ docs/basicInfo.js
@@ -0,0 +1,10 @@
+const index = require('./index.js')
+
+module.exports = {
+    openapi:"3.0.3",
+    info:{
+        version:"1.0.0",
+        title:"Tasks",
+        description: "Task API"
+    }
+}
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const { dbConnection } = require("./config/config")
+const swaggerUI = require('swagger-ui-express')
+const docs = require('./docs/index')
 
 app.use(express.json())
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs))
+
 
 dbConnection()
 
